60b8f87d6a9f9692ebbf3578b18a95996bb090f3
Samet İrkören <sametirkoren@gmail.com>
2020-11-13 07:24:45 +0300

    Add project files.

---
+++ BandAPI/Entities/Album.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BandAPI.Entities
+{
+    public class Album
+    {
+        public Guid Id { get; set; }
+
+        public string Title { get; set; }
+        public string Description { get; set; }
+
+        public Band Band { get; set; }
+
+        public Guid BandId { get; set; }
+    }
+}
---
+++ BandAPI/Entities/Band.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BandAPI.Entities
+{
+    public class Band
+    {
+        public Guid Id { get; set; }
+
+        public string Name { get; set; }
+        public DateTime Founded { get; set; }
+
+        public string MainGenre { get; set; }
+
+        public ICollection<Album> Albums { get; set; } = new List<Album>();
+    }
+}
