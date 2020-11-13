25bd8741ca0c65ba8e37df1392fa5d039c34a0e3
Samet İrkören <sametirkoren@gmail.com>
2020-11-13 07:28:23 +0300

    Data annotation

--- BandAPI/Entities/Band.cs
+++ BandAPI/Entities/Band.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,11 +8,15 @@ namespace BandAPI.Entities
 {
     public class Band
     {
+        [Key]
         public Guid Id { get; set; }
 
+        [Required]
+        [MaxLength(100)]
         public string Name { get; set; }
         public DateTime Founded { get; set; }
-
+        [Required]
+        [MaxLength(50)]
         public string MainGenre { get; set; }
 
         public ICollection<Album> Albums { get; set; } = new List<Album>();
